e0eb2a62b1fba676e737b8147d9d2cd9ecf77f40
AlaaAbdulwahab999 <alaaabdulwahab999@gmail.com>
2024-12-24 22:45:04 +0300

    Final Graduation Project

---
+++ api/slotExit.php
@@ -0,0 +1,8 @@
+<?php
+    require_once('../database.php');
+
+    DATABASE::connect('localhost', 'smartparking', '123456', 'smartparking');
+
+    DATABASE::query("UPDATE slots set status_of_slot=NULL WHERE slot_number=\"" . $_GET['slot_number'] . "\"");
+?>
+
